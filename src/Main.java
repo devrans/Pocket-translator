62b1d93297465415dd9a073bb408bf8333c9f07d
Jakub <xenofusionwow@gmail.com>
2018-08-21 15:56:51 +0200

    added start window frame

--- src/Main.java
+++ src/Main.java
@@ -33,11 +33,14 @@ public class Main {
     private static boolean windowRun = false;
     private static int screenWidth;
     private static int screenHeight;
+    private JLabel sbar;
 
     public static void main(String[] args) throws IOException {
         Main man = new Main();
+        man.startWindow();
         man.getScreenSize();
         man.backProcess();
+
 
     }
 
@@ -70,12 +73,15 @@ public class Main {
 
         keyboardHook.addKeyListener(new GlobalKeyAdapter() {
             @Override public void keyPressed(GlobalKeyEvent event) {
-                // System.out.println(event);
-                if(event.getVirtualKeyCode()==GlobalKeyEvent.VK_ESCAPE)
+                //System.out.println(event.getVirtualKeyCode());
+                if(event.getVirtualKeyCode()==event.VK_OEM_3 && event.isControlPressed() && event.isMenuPressed())
                     run = false;
+
                 //keys to launch translate
                 if(event.isShiftPressed() && event.isControlPressed() && event.isMenuPressed()) {
                     System.out.println("ctrl shift alt");
+                    int check = '`';
+                    System.out.println(check);
                     System.out.println(getTextFromClipboard());
                         createFrame(getTextFromClipboard());
 
@@ -185,7 +191,29 @@ public class Main {
     }
 
 
+    public void startWindow(){
+        JFrame sFrame = new JFrame("Pocket translator");
+        ButtonGroup group = new ButtonGroup();
 
+        JRadioButton rb1 = new JRadioButton("Easy", true);
+        JRadioButton rb2 = new JRadioButton("Medium");
+        JRadioButton rb3 = new JRadioButton("Hard");
+
+        group.add(rb1);
+        group.add(rb2);
+        group.add(rb3);
+
+        sbar = new JLabel("selected");
+
+        sFrame.setSize(350, 250);
+        sFrame.setTitle("Radio buttons");
+        sFrame.setLocationRelativeTo(null);
+        sFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        sFrame.setVisible(true);
+
+
+    }
 
 
 }
